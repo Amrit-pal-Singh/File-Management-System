fe99b47a6931810c345dfbfd7e806d4bddcd7349
Amritpal Singh <2017csb1068@iitrpr.ac.in>
2020-04-07 14:37:41 +0530

    removing unnecessary files

--- API/fileManagement/fileManagementEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/amritpal/Documents/semesters/SEM 6/GEXXX/fileManagement/fileManagementEnv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
