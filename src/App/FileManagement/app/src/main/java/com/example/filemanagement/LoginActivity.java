3be890b11415287bf5dc82410d20c14ff0ac3e69
code-bandit <ripudamansingh1807@gmail.com>
2020-05-03 12:50:08 +0530

    Initial Commit for App

---
+++ src/App/FileManagement/app/src/main/java/com/example/filemanagement/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.filemanagement;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //for changing status bar icon colors
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        setContentView(R.layout.activity_login);
+        changeStatusBarColor();
+    }
+    // Functions
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setStatusBarColor(getResources().getColor(R.color.login_bk_color));
+        }
+    }
+
+    public void onLoginClick(View View) {
+        startActivity(new Intent(this,RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+
+    }
+
+    public void Login(View View) {
+        Toast.makeText(getApplicationContext(), "Trying to Login", Toast.LENGTH_SHORT).show();
+    }
+}
