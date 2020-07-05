a056d7a9eef96bd5f78f198e9e6c518a72cf7a9b
code-bandit <45354471+code-bandit@users.noreply.github.com>
2020-07-05 15:22:17 +0530

    intent to open new page from plan to send batch processing

--- src/App/FileManagement/app/src/main/java/com/example/filemanagement/ui/batchProcessing/BatchProcessingFragment.java
+++ src/App/FileManagement/app/src/main/java/com/example/filemanagement/ui/batchProcessing/BatchProcessingFragment.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.filemanagement.R;
@@ -19,20 +17,29 @@ import com.example.filemanagement.activity_plan_to_send_barcode_bp;
 public class BatchProcessingFragment extends Fragment {
 
     private BatchProcessingViewModel batchProcessingViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        batchProcessingViewModel =
-                ViewModelProviders.of(this).get(BatchProcessingViewModel.class);
+    private Button plant_to_send_btn = null;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        batchProcessingViewModel = ViewModelProviders.of(this).get(BatchProcessingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_batch_processing, container, false);
-        final TextView textView = root.findViewById(R.id.textBPO);
-        batchProcessingViewModel.getText().observe(this, new Observer<String>() {
+//        final TextView textView = root.findViewById(R.id.textBPO);
+//        batchProcessingViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        //-------------------------------------jai aahh part dekhli---------------------------------------------------
+        plant_to_send_btn = root.findViewById(R.id.plan_to_send_btn_bp);
+        plant_to_send_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), activity_plan_to_send_barcode_bp.class);
+                startActivity(intent);
             }
         });
 
+        //-----------------------------------------------------------------------------------------------------------------
         return root;
     }
 }
