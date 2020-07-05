38b8ab79e7ddc9739ff0c4f5e67ecfa47834101f
code-bandit <45354471+code-bandit@users.noreply.github.com>
2020-07-05 15:22:32 +0530

    intent to open new page from plan to send batch processing

--- src/App/FileManagement/app/src/main/java/com/example/filemanagement/ui/home/HomeFragment.java
+++ src/App/FileManagement/app/src/main/java/com/example/filemanagement/ui/home/HomeFragment.java
@@ -10,13 +10,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.filemanagement.ActivityViewFile;
@@ -33,10 +29,8 @@ public class HomeFragment extends Fragment {
     private Spinner roleSelectorSpinner;
     private Button viewFiles;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 //        final TextView textView = root.findViewById(R.id.text_home);
 //        homeViewModel.getText().observe(this, new Observer<String>() {
