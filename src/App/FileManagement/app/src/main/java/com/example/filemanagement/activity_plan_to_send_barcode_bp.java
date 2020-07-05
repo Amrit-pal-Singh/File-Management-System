bf5b20965fa25f4fc24e425f071f679c6bde97ec
Jai Garg <jaigarg1499@gmail.com>
2020-07-05 14:14:16 +0530

    Test commit

---
+++ src/App/FileManagement/app/src/main/java/com/example/filemanagement/activity_plan_to_send_barcode_bp.java
@@ -0,0 +1,14 @@
+package com.example.filemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_plan_to_send_barcode_bp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan_to_send_barcode_bp);
+    }
+}
--- src/App/FileManagement/app/src/main/java/com/example/filemanagement/ui/batchProcessing/BatchProcessingFragment.java
+++ src/App/FileManagement/app/src/main/java/com/example/filemanagement/ui/batchProcessing/BatchProcessingFragment.java
@@ -1,5 +1,6 @@
 package com.example.filemanagement.ui.batchProcessing;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.filemanagement.R;
+import com.example.filemanagement.activity_plan_to_send_barcode_bp;
 
 public class BatchProcessingFragment extends Fragment {
 
@@ -23,13 +25,14 @@ public class BatchProcessingFragment extends Fragment {
         batchProcessingViewModel =
                 ViewModelProviders.of(this).get(BatchProcessingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_batch_processing, container, false);
-        final TextView textView = root.findViewById(R.id.text_batch_processing);
+        final TextView textView = root.findViewById(R.id.textBPO);
         batchProcessingViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
         return root;
     }
 }
