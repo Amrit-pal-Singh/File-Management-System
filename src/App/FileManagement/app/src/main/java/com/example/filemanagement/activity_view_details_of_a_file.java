0e8035fecd4de10d811cff47d50a3967b2092a39
Jai Garg <jaigarg1499@gmail.com>
2020-08-17 18:16:54 +0530

    Restructed layout of viewing details of a file and some bug fixes

--- src/App/FileManagement/app/src/main/java/com/example/filemanagement/activity_view_details_of_a_file.java
+++ src/App/FileManagement/app/src/main/java/com/example/filemanagement/activity_view_details_of_a_file.java
@@ -3,6 +3,7 @@ package com.example.filemanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_view_details_of_a_file extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class activity_view_details_of_a_file extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_details_of_a_file);
+
+        TextView tvQR = (TextView) findViewById(R.id.qr);
+        tvQR.setText("1234");
+
+        TextView tvName = (TextView) findViewById(R.id.name);
+        tvName.setText("Grade Submission File");
     }
 }
