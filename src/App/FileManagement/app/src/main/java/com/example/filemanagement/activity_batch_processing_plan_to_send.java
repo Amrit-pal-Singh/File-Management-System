715ca935a218c996058d8be9247c26f0118ffb24
code-bandit <45354471+code-bandit@users.noreply.github.com>
2020-08-18 13:56:15 +0530

    Plan To Send Spinner Added

--- src/App/FileManagement/app/src/main/java/com/example/filemanagement/activity_batch_processing_plan_to_send.java
+++ src/App/FileManagement/app/src/main/java/com/example/filemanagement/activity_batch_processing_plan_to_send.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
@@ -15,12 +16,19 @@ public class activity_batch_processing_plan_to_send extends Activity {
         setContentView(R.layout.activity_batch_processing_plan_to_send);
 
         ArrayList<String> scannedData = new ArrayList<>();
+
         if(getIntent().getSerializableExtra("BarcodeData") != null) {
             scannedData = (ArrayList<String>) getIntent().getSerializableExtra("BarcodeData");
         }
+
         ListView listView = findViewById(R.id.listViewPlanToSendBPActivity);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, scannedData);
         listView.setAdapter(adapter);
+
+        ArrayList<String> roles = new ArrayList<>();
+        Spinner spinner = findViewById(R.id.spinnerPlanToSendBP);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, roles);
+        spinner.setAdapter(spinnerAdapter);
     }
 
 }
